ba485b2426152edfb4920e070f926ed037a712e8
Phuc Nguyen Hoang <87425340+HPhucDev@users.noreply.github.com>
2021-08-30 12:13:00 +0700

    Update Navbar With Responsive

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,9 @@ const switchElement = document.querySelector('.switch')
 switchElement.addEventListener('click', () => {
   document.body.classList.toggle('dark')
 })
+
+const switchElement_mobile = document.querySelector('.switch__mobile')
+
+switchElement_mobile.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
